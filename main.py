6bdc8e1a28f19e972d55e6e127b9fb9eb57b4222
LeiYangGH <675686066@qq.com>
2020-06-21 21:30:23 +0800

    tk not show well

--- main.py
+++ main.py
@@ -20,18 +20,22 @@ def read_img_array(file_name):
     return arr
 
 
+header_array = [0x42, 0x4D, 0x26, 0x90, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00,
+                0x00,
+                0x00, 0x1C, 0x04, 0x00, 0x00, 0x0C, 0x03, 0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00, 0x00,
+                0x00,
+                0xF0, 0x8F, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00,
+                0x00, 0x00, 0x00]
+
+
 def raw_array_to_bmp_1(img_array, file_name):
     print(len(img_array))
     # 这个头是我用画图工具创建了个1052, 780的bmp文件，
     # 然后复制出来的头信息
-    header_array = [0x42, 0x4D, 0x26, 0x90, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00,
-                    0x00,
-                    0x00, 0x1C, 0x04, 0x00, 0x00, 0x0C, 0x03, 0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00, 0x00,
-                    0x00,
-                    0xF0, 0x8F, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                    0x00,
-                    0x00, 0x00, 0x00]
+
     with  open(file_name, "wb") as fw:
+        global header_array
         for h in header_array:
             fw.write(bytes([h]))
         for b in img_array:
@@ -53,7 +57,27 @@ def raw_array_to_bmp_2(img_array, file_name):
     print(f'wrote to {file_name}')
 
 
+def show_img_array_tk(img_array):
+    import tkinter as tk
+    import numpy as np
+    from PIL import Image, ImageTk
+    root = tk.Tk()
+    # array_with_header = []
+    # global header_array
+    # array_with_header.extend(header_array)
+    # array_with_header.extend(img_array)
+    array = np.array(img_array).reshape(1052 * 3, 780)
+    img = ImageTk.PhotoImage(image=Image.fromarray(array, mode='RGB'))
+
+    canvas = tk.Canvas(root, width=800, height=800)
+    canvas.pack()
+    canvas.create_image(0, 0, anchor="nw", image=img)
+
+    root.mainloop()
+
+
 if __name__ == '__main__':
     img_array = read_img_array(r'C:\Users\LeiYang\Downloads\data.txt')
-    raw_array_to_bmp_1(img_array, r'C:\Users\LeiYang\Downloads\addheader.bmp')
-    raw_array_to_bmp_2(img_array, r'C:\Users\LeiYang\Downloads\pilsave.bmp')
+    # raw_array_to_bmp_1(img_array, r'C:\Users\LeiYang\Downloads\addheader.bmp')
+    # raw_array_to_bmp_2(img_array, r'C:\Users\LeiYang\Downloads\pilsave.bmp')
+    show_img_array_tk(img_array)
